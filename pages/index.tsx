6f38ba1bffe6e0190d32a925b17a7f22fa79141c
Mati <matiasfornes@gmail.com>
2020-09-09 02:33:01 +0200

    chore: github link

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,11 @@ import Link from 'next/link'
 const Home = () => (
   <>
     <h3>{'currency-exchange'}</h3>
+    <pre>
+      <Link href="https://github.com/sombreroEnPuntas/currency-exchange">
+        {'Check the code on github!'}
+      </Link>
+    </pre>
     <Link href="/exchange">{'Exchange'}</Link>
   </>
 )
